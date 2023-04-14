2f2110f4c531878c36b209cc7ce8da6c2b4a8e81
Anastasia Tuzikova <anastasia.tuzikova@zensoft.io>
2023-04-14 11:50:10 +0600

    likes

--- blog/post/serializers.py
+++ blog/post/serializers.py
@@ -6,3 +6,9 @@ class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+    def to_representation(self, instance:Post):
+        rep = super().to_representation(instance)
+        # rep['likes'] = Like.objects.filter(post=instance).count()
+        rep['likes'] = instance.likes.all().count()
+        return rep
